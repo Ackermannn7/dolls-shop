1ed36c0bc70b65995ee540630cc1fd07ca66ac9e
Aleksey Rudyk <aleksey.rudyksm@gmail.com>
2024-11-30 15:36:19 +0100

    Orders CRUD done

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,7 @@
 import express, { json, urlencoded, Request } from 'express';
 import dollsRoutes from './routes/dolls/index.js';
 import authRoutes from './routes/auth/index.js';
-// import ordersRoutes from './routes/orders/index.js';
+import ordersRoutes from './routes/orders/index.js';
 // import stripeRoutes from './routes/stripe/index.js';
 
 import serverless from 'serverless-http';
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 
 app.use('/dolls', dollsRoutes);
 app.use('/auth', authRoutes);
-// app.use('/orders', ordersRoutes);
+app.use('/orders', ordersRoutes);
 // app.use('/stripe', stripeRoutes);
 
 if (process.env.NODE_ENV === 'dev') {
